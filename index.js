9734c5a0a4ebbf90475acf60a4d63a9543014b2d
amising <amising@microsoft.com>
2016-06-24 15:46:42 +0530

    first commit

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
+console.log("app.js:",process.toLocaleString());
 
 app.get('/', function(request, response) {
-  response.send('Hello World! MSFT')
+  response.send('Hello World! MSFT:x-code='+process.env.x-code);
 })
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
+
